07d327b34b944fcf522ce48cafe9f538c669845e
Jokubas Ramanauskas <jokubas.rama@gmail.com>
2014-03-30 14:15:22 +0300

    project init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace os
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

eec16d8ff5c2deab0b5142c6c580843cf3f04971
Jokubas Ramanauskas <jokubas.rama@gmail.com>
2014-04-07 22:38:05 +0300

    ai, hujnia kazkokia

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using os.RealMachine;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VM = os.VirtualMachine;
 
 namespace os
 {
@@ -12,7 +14,20 @@ namespace os
         static void Main(string[] args)
         {
             CPU cpu = new CPU();
-            
+            VM.VirtualMachine virtualMachine = new VM.VirtualMachine();
+
+            Console.WriteLine("Hello, my darling. Please, enter something:");
+            string program = Console.ReadLine();
+            Console.WriteLine("Oh you naughty boy, oh you!");
+
+            /*
+             * Nu vo, dabar reik kazkaip nuskaityt tipo sita faila. Sukelt ji VM atminti.
+             */
+
+            StreamReader streamReader = new StreamReader(program);
+            string fileContent = streamReader.ReadToEnd();
+
+
         }
     }
 }
